190ed802e574bb1e3e546213279ba68a61642518
Hugo Gois <gudshugo@gmail.com>
2024-05-14 22:55:23 -0300

    first commit

---
+++ src/main/java/br/com/projetos/gametracker/GametrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetos.gametracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetos/gametracker/GametrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos.gametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
